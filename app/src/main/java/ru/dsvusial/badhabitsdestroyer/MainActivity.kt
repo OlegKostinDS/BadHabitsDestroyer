6abacc00d96d31ce45d3c2437a077899552c8c5b
Oleg <olegkostinds@gmail.com>
2022-08-04 17:50:10 +0300

    drawer and toolbar

--- app/src/main/java/ru/dsvusial/badhabitsdestroyer/MainActivity.kt
+++ app/src/main/java/ru/dsvusial/badhabitsdestroyer/MainActivity.kt
@@ -2,10 +2,61 @@ package ru.dsvusial.badhabitsdestroyer
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.os.Handler
+import android.os.Looper
+import android.view.Menu
+import android.view.MenuItem
+import androidx.navigation.findNavController
+import androidx.navigation.fragment.NavHostFragment
+import androidx.navigation.ui.AppBarConfiguration
+import androidx.navigation.ui.NavigationUI
+import androidx.navigation.ui.NavigationUI.setupWithNavController
+import androidx.navigation.ui.onNavDestinationSelected
+import androidx.navigation.ui.setupWithNavController
+import com.google.android.material.appbar.MaterialToolbar
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.delay
+import kotlinx.coroutines.launch
+import ru.dsvusial.badhabitsdestroyer.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityMainBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(R.layout.fragment_splash)
+
+        Handler(Looper.myLooper()!!).postDelayed({
+            setContentView(binding.root)
+
+            setSupportActionBar(binding.toolbar)
+            // getting reference to the navigation controller from navigation host
+            val navHostFragment = supportFragmentManager
+                .findFragmentById(R.id.nav_host_fragment)
+                    as NavHostFragment
+            val navController = navHostFragment.navController
+            //enable vanigation when item clikced by linking the drawer to the navigation controller
+            //setupWithNavController method links the drawers navigation view to the navigation controller
+            val navView = binding.navView
+            NavigationUI.setupWithNavController(navView, navController)
+            // builds a configuration that links the toolbar to the navigatin graph
+            // and applies it to the toolbar
+            val builder = AppBarConfiguration.Builder(navController.graph)
+            val appBarConfiguration = builder.build()
+            binding.toolbar.setupWithNavController(navController, appBarConfiguration)
+
+        }, 1000)
+    }
+
+    override fun onCreateOptionsMenu(menu: Menu?): Boolean {
+        menuInflater.inflate(R.menu.menu_toolbar, menu)
+        return super.onCreateOptionsMenu(menu)
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        val navController = findNavController(R.id.nav_host_fragment)
+        return item.onNavDestinationSelected(navController) ||
+        return super.onOptionsItemSelected(item)
     }
 }
--- app/src/main/java/ru/dsvusial/badhabitsdestroyer/ui/main/MainFragment.kt
+++ app/src/main/java/ru/dsvusial/badhabitsdestroyer/ui/main/MainFragment.kt
@@ -3,6 +3,7 @@ package ru.dsvusial.badhabitsdestroyer.ui.main
 import android.os.Bundle
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
+import android.view.Menu
 import android.view.View
 import android.view.ViewGroup
 import ru.dsvusial.badhabitsdestroyer.R
@@ -10,17 +11,28 @@ import ru.dsvusial.badhabitsdestroyer.databinding.FragmentMainBinding
 
 
 class MainFragment : Fragment() {
+    // view binding references
     private var _binding: FragmentMainBinding? = null
     private val binding get() = _binding!!
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
-    ): View? {
+    ): View {
         //initialization of binding, get access to view root
         _binding = FragmentMainBinding.inflate(inflater, container, false)
-        return binding.root
+        val view = binding.root
+
+
+        return view
     }
 
+
+
+//    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+//        val navController = findNavController(R.id.nav_host_fragment)
+//        return item.onNavDestinationSelected(navController) ||
+//        return super.onOptionsItemSelected(item)
+//    }
 
 }
